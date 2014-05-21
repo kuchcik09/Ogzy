f5cb99ee0263b864a291c2b0b52f03a0875ab058
marcinMarcin <marcin.gorlice@gmail.com>
2014-05-21 21:17:03 +0200

    Taby dla obecności i ocen.

--- RibbonModule/src/org/officelaf/ribbon/obecnosc/SprawdzObecnoscAction.java
+++ RibbonModule/src/org/officelaf/ribbon/obecnosc/SprawdzObecnoscAction.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.officelaf.ribbon.obecnosc;
 
 import org.database.models.Termin;
@@ -14,66 +8,84 @@ import javax.swing.JOptionPane;
 import org.gui.MainTopComponent;
 import org.gui.grupy_cwiczeniowe.GrupaCwiczeniowaTopComponent;
 import org.gui.obecnosci.PresenceTableTopComponent;
+import org.openide.windows.Mode;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
 /**
  *
  * @author Duży
+ * @edited Marcin
  */
-public class SprawdzObecnoscAction extends AbstractAction{
+public class SprawdzObecnoscAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
         TopComponent top = TopComponent.getRegistry().getActivated();
         MainTopComponent maintop = null;
         GrupaCwiczeniowaTopComponent grouptop = null;
-        if(top instanceof MainTopComponent) maintop = (MainTopComponent) top;
-        else if(top instanceof GrupaCwiczeniowaTopComponent) grouptop = (GrupaCwiczeniowaTopComponent) top;
-        if(maintop != null && maintop.getTable().getSelectedRow() != -1 && maintop.getTable().getSelectedColumn() > 0 && maintop.getTableModel().getValueAt(maintop.getTable().getSelectedRow(), maintop.getTable().getSelectedColumn()) != null){
+        if (top instanceof MainTopComponent) {
+            maintop = (MainTopComponent) top;
+        } else if (top instanceof GrupaCwiczeniowaTopComponent) {
+            grouptop = (GrupaCwiczeniowaTopComponent) top;
+        }
+        if (maintop != null && maintop.getTable().getSelectedRow() != -1 && maintop.getTable().getSelectedColumn() > 0 && maintop.getTableModel().getValueAt(maintop.getTable().getSelectedRow(), maintop.getTable().getSelectedColumn()) != null) {
             String data = (String) maintop.getTableModel().getValueAt(maintop.getTable().getSelectedRow(), 0);
             String godzina_start = data.substring(0, 5);
             String godzina_stop = data.substring(8, data.length());
-            Termin.DZIEN_TYG tyg = Termin.DZIEN_TYG.values()[maintop.getTable().getSelectedColumn()-1];
+            Termin.DZIEN_TYG tyg = Termin.DZIEN_TYG.values()[maintop.getTable().getSelectedColumn() - 1];
             LinkedList<Termin> terms = Termin.getAllTerms();
             Termin term = null;
-            for(Termin t: terms){
-                if(t.getGodzina_start().equals(godzina_start) && t.getGodzina_stop().equals(godzina_stop) && t.getDzien_tygodnia()==tyg){
+            for (Termin t : terms) {
+                if (t.getGodzina_start().equals(godzina_start) && t.getGodzina_stop().equals(godzina_stop) && t.getDzien_tygodnia() == tyg) {
                     term = t;
                     break;
                 }
             }
             //w term jest teraz zaznaczony termin
-            PresenceTableTopComponent ptop = (PresenceTableTopComponent) WindowManager.getDefault().findTopComponent("PresenceTableTopComponent");
-            ptop.setTerm(term);
-            ptop.open();
-            ptop.requestActive();
-        }else if(grouptop !=null && grouptop.getTable().getSelectedRow() != -1){
+            openPresenceTableTopComponent(term);
+        } else if (grouptop != null && grouptop.getTable().getSelectedRow() != -1) {
             String data = (String) grouptop.getTableModel().getValueAt(grouptop.getTable().getSelectedRow(), 1);
             String godzina_start = data.substring(0, 5);
             String godzina_stop = data.substring(8, data.length());
             Termin.DZIEN_TYG tyg = Termin.DZIEN_TYG.valueOf((String) grouptop.getTableModel().getValueAt(grouptop.getTable().getSelectedRow(), 0));
             LinkedList<Termin> terms = Termin.getAllTerms();
             Termin term = null;
-            for(Termin t: terms){
-                if(t.getGodzina_start().equals(godzina_start) && t.getGodzina_stop().equals(godzina_stop) && t.getDzien_tygodnia()==tyg){
+            for (Termin t : terms) {
+                if (t.getGodzina_start().equals(godzina_start) && t.getGodzina_stop().equals(godzina_stop) && t.getDzien_tygodnia() == tyg) {
                     term = t;
                     break;
                 }
             }
             // tu znajduje sie termin wypełniony
-            PresenceTableTopComponent ptop = (PresenceTableTopComponent) WindowManager.getDefault().findTopComponent("PresenceTableTopComponent");
-            ptop.setTerm(term);
-            ptop.open();
-            ptop.requestActive();
-        }
-        else if(top instanceof PresenceTableTopComponent){
+            openPresenceTableTopComponent(term);
+        } else if (top instanceof PresenceTableTopComponent) {
             JOptionPane.showMessageDialog(maintop, "Już jesteś we właściwym oknie głupku :D", "Obecności - BŁĄD", JOptionPane.ERROR_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(maintop, "Wybierz termin na planie zajęć lub w bezpośrednio w grupie aby sprawdzić dla niego obecność", "Obecności - BŁĄD", JOptionPane.ERROR_MESSAGE);
         }
-        else{
-            JOptionPane.showMessageDialog(maintop, "Wybierz termin na planie zajęć lub w bezpośrednio w grupe aby sprawdzić dla niego obecność", "Obecności - BŁĄD", JOptionPane.ERROR_MESSAGE);
-        }
-        
+
     }
-    
+
+    private void openPresenceTableTopComponent(Termin term) {
+
+        Mode mode = WindowManager.getDefault().findMode("editor");
+        for (TopComponent tc : WindowManager.getDefault().getOpenedTopComponents(mode)) {
+            if (tc instanceof PresenceTableTopComponent) {
+                if (((PresenceTableTopComponent) tc).getTermId() == term.getId()) {
+                    if (!tc.isOpened()) {
+                        tc.open();
+                    }
+                    tc.requestActive();
+                    return;
+                }
+            }
+        }
+        PresenceTableTopComponent ptop = new PresenceTableTopComponent();
+
+        ptop.setTerm(term);
+        ptop.open();
+        ptop.requestActive();
+    }
+
 }
